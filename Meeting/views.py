7f724bc3ac176c96f2b7a21e66544e54d7aee1ba
miracle <jihao.lu@qq.com>
2019-06-14 22:34:54 +0800

    登录和注册逻辑完成！验证通过

--- Meeting/views.py
+++ Meeting/views.py
@@ -1,8 +1,9 @@
 """django 视图模块，通过它可以把用户请求的页面调出来。"""
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import *
 from Meeting.models import *
@@ -10,9 +11,9 @@ from Meeting.models import *
 
 # 首页
 def index(req):
-    username = req.session.get('username', '')
-    content = {'user': username}
-    return render_to_response('index.html', content)  # 将数据返回到前台
+    if req.session.get('user'):
+        return render(req, 'index.html')
+    return redirect('/login/')
 
 
 # 注册
@@ -20,44 +21,61 @@ def register(req):
     if req.method == 'GET':
         return render(req, 'register.html')
     if req.method == 'POST':
-        email = req.POST.get("id_Email")
-        name = req.POST.get("id_Name")
-        if Users.objects.filter(email=email, username=name):
-            state = "user_exist"
-            return render(req, 'register.html', {'state': state})
+        id_email = req.POST.get("id_Email")
+        id_name = req.POST.get("id_Name")
+        # 判断用户输入的邮箱和用户名是否存在
+        email = Users.objects.filter(email=id_email)
+        name = Users.objects.filter(username=id_name)
+        if email and name:
+            message = '用户名或者邮箱已经被注册，请重新输入！'
+            return render(req, 'register.html', {'message': message})
         else:
-            password = req.POST.get("id_Password")
-            phone = req.POST.get("id_Phone")
-            new_DB_user = Users.objects.create(email=email, password=password, username=name, phone=phone)
-            new_DB_User = User.objects.create_user(email=email, password=password, username=name)
+            id_password = req.POST.get("id_Password")
+            id_phone = req.POST.get("id_Phone")
+            new_DB_user = Users.objects.create(email=id_email, password=id_password, username=id_name, phone=id_phone)
+            new_DB_User = User.objects.create_user(username=id_name, email=id_email, password=id_password)
             new_DB_user.save()
             new_DB_User.save()
-            state = 'success'
-            return render(req, 'login.html', {'state': state})
+            print(new_DB_User)
+            message = '用户注册成功,请登录。'
+            return render(req, 'login.html', {'message': message})
 
 
 # 登录
 def login(req):
+    if req.session.get('is_login', None):  # 不允许重复登录
+        return redirect('/index/')
     if req.method == 'GET':
         return render(req, 'login.html')
     if req.method == 'POST':
-        username = req.POST.get('id_Username')
-        password = req.POST.get('id_Password')
-        # 数据校验,用户校验
-        user = authenticate(username=username, password=password)
-        if user:
-            # 用户登录
-            login(request=req, user=user)
-            # 登录成功返回页面
-            return render(req, 'index.html', {'state': 'success', 'message': '登录成功'})
+        name = req.POST.get('id_Username')
+        passwd = req.POST.get('id_Password')
+        message = '用户名或者密码为空！'
+        if name.strip() and passwd:
+            # 用户名字符合法性验证
+            # 密码长度验证
+            # 更多的其它验证.....
+            try:
+                user = Users.objects.get(username=name)
+            except :
+                message = '用户不存在！'
+                return render(req, 'login.html', {'message': message})
+            if user.password == passwd:
+                req.session['is_login'] = True
+                req.session['user'] = name
+                return render(req, 'index.html', {'user': name})
+            else:
+                message = '密码不正确！'
+                return render(req, 'login.html', {'message': message})
         else:
-            return render(req, 'login.html', {'state': 'fail', 'message': '用户不存在或在密码错误，'})
+            return render(req, 'login.html', {'message': message})
+    return render(req, 'login.html', {'message': '用户不存在或在密码错误，'})
 
 
 # 退出登录
 def logout(req):
-    auth.logout(req)
-    return HttpResponseRedirect('/login')
+    req.session.flush()
+    return redirect('/login')
 
 
 # 获取园区列表
